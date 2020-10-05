16d839be62784725a866b23a4045afc84b4dd3e3
Vetty <vetty88@gmail.com>
2020-10-05 14:08:14 +1100

    few js and package json updates

---
+++ client/src/pages/Competitions.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import DeleteBtn from "../components/DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
+
+function Competitions() {
+  // Setting our component's initial state
+  const [competitions, setCompetitions] = useState([])
+  const [formObject, setFormObject] = useState({})
+
+  // Load all Competitions and store them with setCompetitions
+  useEffect(() => {
+    loadCompetitions();
+  }, []);
+
+  // Loads all Competitions and sets them to Competitions
+  function loadCompetitions() {
+    API.getCompetitions()
+      .then(res => 
+        setCompetitions(res.data)
+      )
+      .catch(err => console.log(err));
+  };
+
+  // Deletes a Competition from the database with a given id, then reloads Competitions from the db
+  function deleteCompetition(id) {
+    API.deleteCompetition(id)
+      .then(res => loadCompetitions())
+      .catch(err => console.log(err));
+  }
+
+  // Handles updating component state when the user types into the input field
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({...formObject, [name]: value})
+  };
+
+  // When the form is submitted, use the API.saveCompetition method to save the Competition data
+  // Then reload Competitions from the database
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (formObject.eventName && formObject.horse) {
+      API.saveCompetition({
+        eventName: formObject.eventName,
+        horse: formObject.horse,
+        resultNotes: formObject.resultNotes
+      })
+        .then(res => loadCompetitions())
+        .catch(err => console.log(err));
+    }
+  };
+
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h1>What Competitions Should I Enter?</h1>
+            </Jumbotron>
+            <form>
+              <Input
+                onChange={handleInputChange}
+                name="eventName"
+                placeholder="eventName (required)"
+              />
+              <Input
+                onChange={handleInputChange}
+                name="horse"
+                placeholder="horse (required)"
+              />
+              <TextArea
+                onChange={handleInputChange}
+                name="resultNotes"
+                placeholder="resultNotes (Optional)"
+              />
+              <FormBtn
+                disabled={!(formObject.horse && formObject.eventName)}
+                onClick={handleFormSubmit}
+              >
+                Submit Competition
+              </FormBtn>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
+            <Jumbotron>
+              <h1>Competitions On My List</h1>
+            </Jumbotron>
+            {competitions.length ? (
+              <List>
+                {competitions.map(competition => (
+                  <ListItem key={competition._id}>
+                    <Link to={"/competitions/" + competition._id}>
+                      <strong>
+                        {competition.eventName} by {competition.horse}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => deleteCompetition(competition._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+
+
+export default Competitions;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 2200;
+const PORT = process.env.PORT || 3000;
 
 
 // Define middleware here
