4497a1be23b319cccb2096fc7a4e3e3dc8ffc19c
Yvette Waller <vetty88@gmail.com>
2021-02-14 16:17:41 +1100

    update

---
+++ client/node_modules/@sentry/core/esm/version.js
@@ -0,0 +1,2 @@
+export var SDK_VERSION = '6.1.0';
+//# sourceMappingURL=version.js.map
--- react-ui/react-compsmgr/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const competitionRoutes = require("./competitions");
-
-// Competition routes
-router.use("/competitions", competitionRoutes);
-
-module.exports = router;
