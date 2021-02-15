0541ff2fd8b2eba0987b68fb0883ba8450d9619a
Yvette Waller <vetty88@gmail.com>
2021-02-15 11:18:21 +1100

    upodae

---
+++ amthlete-app/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>Google Books - Y Waller</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Competitions from "./pages/Competitions";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
+import React, { Component } from "react";
+import { ToastContainer } from "react-toastify";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Competitions from "./pages/competitions";
+import Detail from "./pages/detail";
+import NoMatch from "./pages/NnoMatch";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/competitions"]}>
-            <Competitions />
-          </Route>
-          <Route exact path="/competitions/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <section className="hero-is-fullheight">
+          <div className="head-head">
+            <Nav />
+          </div>
+          <div className="head-body">
+            <Switch>
+              <Route path="/competitions" component={Competitions} />
+              <Route path="/detail" component={Detail} />
+              <Route path="/nomatch" component={NoMatch} />
+              <Redirect from="/" exact to="/competitions" />
+              <Redirect to="/nomatch" />
+            </Switch>
+          </div>
+          <div className="head-foot">
+            <Footer />
+          </div>
+        </section>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
