bf2cbdee650b27436b52a6bb5424ed23c47ba4db
Yvette Waller <vetty88@gmail.com>
2021-02-17 10:55:31 +1100

    removed launch json and amthlete app folder

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -8,8 +8,6 @@ import API from "../utils/API";
 function Detail(props) {
   const [competition, setCompetition] = useState({});
 
-  // When this component mounts, grab the competition with the _id of props.match.params.id
-  // e.g. localhost:3000/competitions/599dcb67f0f16317844583fc
   const {id} = useParams();
   useEffect(() => {
     API.getCompetition(id)
