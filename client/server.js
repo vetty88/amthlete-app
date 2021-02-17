bc0ed535c7199c45457fee437b5bf98c36d062e2
Yvette Waller <vetty88@gmail.com>
2021-02-17 11:36:08 +1100

    update to auth admin mongodb conn refused error

--- client/server.js
+++ client/server.js
@@ -21,7 +21,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/equestriancompsdb",
+  process.env.MONGODB_URI || "mongodb://localhost/equestriancompsdb?authSource=admin",
   {
     useCreateIndex: true,
     useNewUrlParser: true
