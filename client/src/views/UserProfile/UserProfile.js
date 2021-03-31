1ae9d358bf1941ab416fe06ac94b4e82309c048e
Yvette Waller <vetty88@gmail.com>
2021-03-31 13:23:25 +1100

    moved comps to user page

--- client/src/views/UserProfile/UserProfile.js
+++ client/src/views/UserProfile/UserProfile.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -13,30 +14,89 @@ import CardAvatar from "../../components/Card/CardAvatar.js";
 import CardBody from "../../components/Card/CardBody.js";
 import CardFooter from "../../components/Card/CardFooter.js";
 
+import DeleteBtn from "../../components/DeleteBtn";
+
+import API from "../../utils/API";
+
+// import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../../components/List";
+import { Input, DateSelector, SelectEvents, SelectHorse, SelectPlacing, TextArea, FormBtn } from "../../components/Form";
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
+
 import avatar from "../../assets/img/faces/marc.jpg";
 
-const styles = {
-  cardCategoryWhite: {
-    color: "rgba(255,255,255,.62)",
-    margin: "0",
-    fontSize: "14px",
-    marginTop: "0",
-    marginBottom: "0"
-  },
-  cardTitleWhite: {
-    color: "#FFFFFF",
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none"
+
+export default function Competitions() {
+  // Setting our component's initial state
+  const [competitions, setCompetitions] = useState([])
+  const [formObject, setFormObject] = useState({})  
+  const styles = {
+    cardCategoryWhite: {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0"
+    },
+    cardTitleWhite: {
+      color: "#FFFFFF",
+      marginTop: "0px",
+      minHeight: "auto",
+      fontWeight: "300",
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+      marginBottom: "3px",
+      textDecoration: "none"
+    }
+  };
+  
+  const useStyles = makeStyles(styles);
+  
+
+  // Load all competitions and store them with setCompetitions
+  useEffect(() => {
+    loadCompetitions()
+  }, [])
+
+  // Loads all competitions and sets them to competitions
+  function loadCompetitions() {
+    API.getCompetitions()
+      .then(res => 
+        setCompetitions(res.data)
+      )
+      .catch(err => console.log(err));
+  };
+
+  // Deletes a competition from the database with a given id, then reloads competitions from the db
+  function deleteCompetition(id) {
+    API.deleteCompetition(id)
+      .then(res => loadCompetitions())
+      .catch(err => console.log(err));
   }
-};
 
-const useStyles = makeStyles(styles);
+  // Handles updating component state when the user types into the input field
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({...formObject, [name]: value})
+  };
 
-export default function UserProfile() {
+  // When the form is submitted, use the API.saveCompetition method to save the competition data
+  // Then reload competitions from the database
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (formObject.eventName && formObject.horse) {
+      API.saveCompetition({
+        eventName: formObject.eventName,
+        horse: formObject.horse,
+        eventType: formObject.eventType,
+        placing: formObject.placing,
+        penalties: formObject.penalties,
+        resultNotes: formObject.resultNotes,
+        date: new Date(formObject.date),
+      })
+        .then(res => loadCompetitions())
+        .catch(err => console.log(err));
+    }
+  };
   const classes = useStyles();
   return (
     <div>
@@ -44,12 +104,62 @@ export default function UserProfile() {
         <GridItem xs={12} sm={12} md={8}>
           <Card>
             <CardHeader color="primary">
-              <h4 className={classes.cardTitleWhite}>Edit Profile</h4>
-              <p className={classes.cardCategoryWhite}>Complete your profile</p>
+              <h4 className={classes.cardTitleWhite}>Add Competition</h4>
+              <p className={classes.cardCategoryWhite}>Add a New Comp! </p>
             </CardHeader>
             <CardBody>
               <GridContainer>
                 <GridItem xs={12} sm={12} md={5}>
+                <form>
+              <Input
+                onChange={handleInputChange}
+                name="eventName"
+                placeholder="EventName (required)"
+              />      
+             <SelectEvents 
+                onChange = {handleInputChange}
+                name = "eventType"
+                placeholder = "EventType (required)"
+              />
+
+              <SelectHorse
+                onChange={handleInputChange}
+                name="horse"
+                placeholder="Horse (required)"
+              />
+
+              <SelectPlacing
+                onChange={handleInputChange}
+                name="placing"
+                placeholder="Placing (required)"
+              />
+
+              <Input
+                onChange={handleInputChange}
+                name="penalties"
+                placeholder="Penalties (required)"
+              />
+              
+              <TextArea
+                onChange={handleInputChange}
+                name="resultNotes"
+                placeholder="Result Notes (Optional)"
+                
+              />
+    
+              <DateSelector
+                onChange={handleInputChange}
+                name="date"
+                placeholder="Comp Date Enter as YYYY-M-DD, default time to 13:00 to save correct date"
+              />
+
+              <FormBtn
+                disabled={!(formObject.horse && formObject.eventName)}
+                onClick={handleFormSubmit}
+              >
+                Submit Competition
+              </FormBtn>
+            </form>
                   <CustomInput
                     labelText="Company (disabled)"
                     id="company-disabled"
@@ -172,6 +282,22 @@ export default function UserProfile() {
             </CardBody>
           </Card>
         </GridItem>
+        {competitions.length ? (
+              <List>
+                {competitions.map(competition => (
+                  <ListItem key={competition._id}>
+                    <Link to={"/competitions/" + competition._id}>
+                      <strong>
+                        {competition.eventName} with {competition.horse}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => deleteCompetition(competition._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
       </GridContainer>
     </div>
   );
