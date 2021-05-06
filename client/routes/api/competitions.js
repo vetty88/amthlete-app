d9fc2c53d7b59d5969b25ae4218aa51abb6fe066
Yvette Waller <vetty88@gmail.com>
2021-05-06 14:21:46 +1000

    update routes

---
+++ client/routes/api/competitions.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const competitionController = require("../../controller/competitionController");
+
+// Matches with "/api/admin/competitions"
+router.route("/")
+  .get(competitionController.findAll)
+  .post(competitionController.create);
+
+// Matches with "/api/admin/competitions/:id"
+router
+  .route("/:id")
+  .get(competitionController.findById)
+  .put(competitionController.update)
+  .delete(competitionController.remove);
+
+module.exports = router;
---
+++ client/routes/api/horses.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const horseController = require("../../controller/horseController");
+
+// Matches with "/api/admin/horses"
+router.route("/")
+  .get(horseController.findAll)
+  .post(horseController.create);
+
+// Matches with "/api/admin/horses/:uniqueName"
+router
+  .route("/:uniqueName")
+  .get(horseController.findByUniqueName)
+  .put(horseController.update)
+  .delete(horseController.remove);
+
+module.exports = router;
