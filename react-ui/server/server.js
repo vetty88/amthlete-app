b2f8b82bd8d5cf9eea9bc61c72831082a2aabffc
Yvette Waller <vetty88@gmail.com>
2021-05-24 14:24:59 +1000

    updates

--- react-ui/server/server.js
+++ react-ui/server/server.js
@@ -41,7 +41,7 @@ app.use("/api/users", users);
 app.use("/api/horses", horses);
 app.use("/api/competitions", competitions);
 
-const server = app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   const port = server.address().port;
   console.log(`Express is working on port ${port}`);
 });
